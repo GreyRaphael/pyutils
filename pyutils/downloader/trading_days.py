3844383c738cdff73461c8d4f046fddd1edf1da9
CloudGeWei <grey@pku.edu.cn>
2023-12-29 10:37:01 +0800

    optimize trading_days downloader with async `httpx`

--- pyutils/downloader/trading_days.py
+++ pyutils/downloader/trading_days.py
@@ -1,6 +1,7 @@
 import sys
 import json
-import requests
+import asyncio
+import httpx
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0",
@@ -8,19 +9,29 @@ HEADERS = {
 }
 
 
+async def fetch_url(client, url):
+    response = await client.get(url, headers=HEADERS, timeout=None)
+    print("↓", url)
+    return response.json().get("data")
+
+
+async def fetch_urls(urls: list):
+    client = httpx.AsyncClient()
+    tasks = [fetch_url(client, url) for url in urls]
+    return await asyncio.gather(*tasks)
+
+
 def download_calendar(year: str, output_dir: str = "."):
     print("↓ trading days of", year)
-    trading_days = []
-    for i in range(1, 13):
-        url = f"http://www.szse.cn/api/report/exchange/onepersistenthour/monthList?month={year}-{i:02d}"
-        print("↓", url)
-        j_data_list = requests.get(url, headers=HEADERS).json().get("data")
-        trading_records = [record["jyrq"] for record in j_data_list if record["jybz"] == "1"]
-        trading_days += trading_records
+    urls = [f"http://www.szse.cn/api/report/exchange/onepersistenthour/monthList?month={year}-{i:02d}" for i in range(1, 13)]
+    j_data_list = asyncio.run(fetch_urls(urls))
+    print("finish ↓ trading days of", year)
 
+    # write to json
+    trading_days = [record["jyrq"] for record_list in j_data_list for record in record_list if record["jybz"] == "1"]
     with open(f"{output_dir}/{year}.json", "w") as file:
         json.dump(trading_days, file, indent=4)
-    print("finish trading days of", year)
+    print("finish writing trading days of", year)
 
 
 if __name__ == "__main__":

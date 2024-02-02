4c3c7e2fae76b002047d0bc2e1497c2a7fe66b19
csc_fedora_gewei <grey@pku.edu.cn>
2024-02-02 16:02:16 +0800

    finish EtfShDownloader

--- pyutils/downloader/etf.py
+++ pyutils/downloader/etf.py
@@ -1,11 +1,14 @@
-import httpx
 import asyncio
 import random
+import os
+import json
+import csv
+import httpx
 
 
 class EtfShDownloader:
     def __init__(self):
-        self._client = httpx.AsyncClient()
+        self._client = httpx.AsyncClient(limits=httpx.Limits(max_connections=20))
         self._headers = self._generate_headers()
 
     def _generate_headers(self) -> dict:
@@ -17,6 +20,7 @@ class EtfShDownloader:
 
     async def _fetch_url(self, url) -> dict:
         response = await self._client.get(url, headers=self._headers, timeout=None)
+        print(f"==> {url}")
         return response.text
 
     async def _fetch_urls(self, urls: list):
@@ -24,7 +28,7 @@ class EtfShDownloader:
         return await asyncio.gather(*tasks)
 
     def _craw_etf_list(self) -> dict:
-        """获取ETF列表, source: http://www.sse.com.cn/assortment/fund/etf/list/"""
+        """获取ETF代码列表, source: http://www.sse.com.cn/assortment/fund/etf/list/"""
         # subClass=01, 沪ETF
         # subClass=03, 沪深京ETF
         # subClass=08, 沪深港京ETF
@@ -32,17 +36,48 @@ class EtfShDownloader:
         # subClass=31, 科创板plus ETF
         subclass_list = ["01", "03", "08", "09", "31"]
         url_list = [f"http://query.sse.com.cn/commonSoaQuery.do?jsonCallBack=jsonpCallback87795546&sqlId=FUND_LIST&fundType=00&subClass={subc}" for subc in subclass_list]
-        txt_list = asyncio.run(self._fetch_urls(url_list))
+
+        loop = asyncio.get_event_loop()
+        txt_list = loop.run_until_complete(self._fetch_urls(url_list))
+
         etf_list = []
-        null = None
         for txt in txt_list:
-            j_data = eval(txt[22:-1])
+            j_data = json.loads(txt[22:-1])
             code_list = [record["fundCode"] for record in j_data["result"]]
             etf_list.extend(code_list)
         return etf_list
 
+    def _craw_etf_details(self, code_list) -> dict:
+        url_list = [f"http://query.sse.com.cn/commonQuery.do?jsonCallBack=jsonpCallback24328638&FUNDID2={code}&sqlId=COMMON_SSE_CP_JJLB_ETFJJGK_GGSGSHQD_COMPONENT_C" for code in code_list]
+
+        loop = asyncio.get_event_loop()
+        txt_list = loop.run_until_complete(self._fetch_urls(url_list))
+
+        all_details = {}
+        for code, txt in zip(code_list, txt_list):
+            j_data = json.loads(txt[22:-1])
+            all_details[code] = j_data["result"]
+        return all_details
+
+    @classmethod
+    def write_details(cls, directory: str, etf_details: dict):
+        os.makedirs(directory, exist_ok=True)
+        for code in etf_details:
+            data_dict = etf_details[code]
+            # 保留成分股数量<=50
+            if len(data_dict) > 50:
+                continue
+            with open(f"{directory}/{code}.csv", "w", encoding="utf8") as f:
+                fields = ["INSTRUMENT_ID", "QUANTITY", "CREATION_PREMIUM_RATE", "REDEMPTION_DISCOUNT_RATE", "SUBSTITUTION_FLAG", "SUBSTITUTION_CASH_AMOUNT"]
+                csv_writer = csv.DictWriter(f, fieldnames=fields, extrasaction="ignore")
+                csv_writer.writeheader()
+                csv_writer.writerows(data_dict)
+
 
 if __name__ == "__main__":
     sh_downloader = EtfShDownloader()
-    etfs = sh_downloader._craw_etf_list()
-    print(etfs)
+    etf_code_list = sh_downloader._craw_etf_list()
+    print("etf length:", len(etf_code_list))
+    etf_details = sh_downloader._craw_etf_details(etf_code_list)
+    EtfShDownloader.write_details("sh_etf", etf_details)
+    print("write_csv done")
